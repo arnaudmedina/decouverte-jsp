e2d1e04d280db65b85185ce0b12139462c973754
arnaudmedina <arnaud.medina@pole-emploi.fr>
2017-09-28 16:18:29 +0200

    Etape 8 : gestion de la liste des utilisateurs saisis

---
+++ src/main/java/co/simplon/poleEmploi/decouverte/jsp/Utilisateur.java
@@ -0,0 +1,46 @@
+package co.simplon.poleEmploi.decouverte.jsp;
+
+public class Utilisateur {
+
+	private String nom;
+	private String prenom;
+	private Utilisateur copain;
+
+	public Utilisateur (String nom, String prenom)
+	{
+		setNom(nom);
+		setPrenom(prenom);
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Utilisateur getCopain() {
+		return copain;
+	}
+
+	public void setCopain(Utilisateur copain) {
+		this.copain = copain;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [" + nom + ", prenom=" + prenom + "]";
+	}
+
+	
+	
+}
